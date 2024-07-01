01c880f686b3f9407ddf61ffd59a73d9a23e5a2f
Stephen Steiner <46692873+stephensteiner4@users.noreply.github.com>
2024-07-01 00:38:18 +0000

    create rock-paper-scissors app

--- app.rb
+++ app.rb
@@ -2,8 +2,55 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  
+  erb(:homepage)
+
+end
+
+get("/scissors") do
+  moves = ["rock", "scissors", "paper"]
+
+  @comp_move = moves.sample
+
+  if @comp_move == "paper"
+    @outcome = "won"
+  elsif @comp_move == "scissors"
+    @outcome = "tied"
+  else
+    @outcome = "lost"
+  end
+
+  erb(:scissors)
+end
+
+get("/rock") do
+  moves = ["rock", "scissors", "paper"]
+
+  @comp_move = moves.sample
+
+  if @comp_move == "paper"
+    @outcome = "lost"
+  elsif @comp_move == "scissors"
+    @outcome = "won"
+  else
+    @outcome = "tied"
+  end
+
+erb(:rock)
+end
+
+get("/paper") do
+  moves = ["rock", "scissors", "paper"]
+
+  @comp_move = moves.sample
+
+  if @comp_move == "paper"
+    @outcome = "tied"
+  elsif @comp_move == "scissors"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+
+  erb(:paper)
 end
